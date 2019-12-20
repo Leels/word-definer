f42bda8b3d6583dfe8dce812590a047cb1112cc1
Lela Smith <smith.lelaa@gmail.com>
2019-12-20 10:20:18 -0800

    add code to pass specs to create and save words

--- lib/word.rb
+++ lib/word.rb
@@ -9,6 +9,21 @@ class Word
     @id = id || @@total_rows += 1
   end
 
+  def self.all
+    @@words.values
+  end
 
+  def self.clear
+    @@words = {}
+    @@total_rows = 0
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.word, self.id)
+  end
+
+  def ==(word_to_compare)
+    self.word == word_to_compare.word
+  end
 
 end
