24843bd62bfc5ddfd5c01484222bba3f1ebde872
Lela Smith <smith.lelaa@gmail.com>
2019-12-20 12:35:51 -0800

    add routing code to app.rb

---
+++ app.rb
@@ -0,0 +1,55 @@
+require('sinatra/reloader')
+require('./lib/word')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  if params["search"]
+    @words = Word.search_by_word_name(params[:search])
+  else
+    @words = Word.all
+  end
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  word_name = params[:word_name]
+  definition = params[:definition]
+  word = Word.new(name, nil, definition)
+  word.save()
+  @words = Word.all()
+  erb(:words)
+end
+
+get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  erb(:word)
+end
+
+get('/words/:id/edit') do
+  @word = Word.find(params[:id].to_i())
+  erb(:edit_word)
+end
+
+patch('/words/:id') do
+    @word = Word.find(params[:id].to_i())
+    @word.update(params[:name], params[:definition])
+  @words = Word.all
+  erb(:words)
+end
+
+delete('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  @words = Word.all
+  erb(:words)
+end
