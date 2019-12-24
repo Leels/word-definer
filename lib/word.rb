0c7d96dac43a55aef4233bdb060a566cf5156994
Lela Smith <smith.lelaa@gmail.com>
2019-12-24 14:58:45 -0800

    add code to create new class for definitions and pass specs

--- lib/word.rb
+++ lib/word.rb
@@ -1,13 +1,12 @@
 class Word
-  attr_accessor :word_name, :id, :definition, :image
+  attr_accessor :word_name, :id, :image
 
   @@words = {}
   @@total_rows = 0
 
-  def initialize(word_name, id, definition, image)
+  def initialize(word_name, id, image)
     @word_name = word_name
     @id = id || @@total_rows += 1
-    @definition = definition
     @image = image
   end
 
@@ -21,7 +20,7 @@ class Word
   end
 
   def save
-    @@words[self.id] = Word.new(self.word_name, self.id, self.definition, self.image)
+    @@words[self.id] = Word.new(self.word_name, self.id, self.image)
   end
 
   def ==(word_to_compare)
@@ -36,11 +35,10 @@ class Word
     @@words.delete(self.id)
   end
 
-  def update(word_name, definition, image)
+  def update(word_name, image)
    self.word_name = word_name
-   self.definition = definition
    self.image = image
-   @@words[self.id] = Word.new(self.word_name, self.id, self.definition, self.image)
+   @@words[self.id] = Word.new(self.word_name, self.id, self.image)
  end
 
  def self.search_by_word_name(word_name)
@@ -50,4 +48,9 @@ class Word
   def self.alphabetize
      @@words.values().sort { |a, b| a.word_name.downcase <=> b.word_name.downcase }
    end
+
+   def definitions
+    Definition.find_by_word(self.id)
+  end
+
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,10 +1,13 @@
-require('rspec')
-require('word')
+require 'rspec'
+require 'definition'
+require 'word'
+require 'pry'
 
 describe '#Word' do
 
   before(:each) do
     Word.clear()
+    Definition.clear()
   end
 
   describe('.all') do
@@ -15,9 +18,9 @@ describe '#Word' do
 
   describe('.clear') do
       it("clears all words") do
-        word = Word.new("Abc", nil, "blah blah", "img")
+        word = Word.new("Abc", nil, "img")
         word.save()
-        word2 = Word.new("Xyz", nil, "yada yada", "img")
+        word2 = Word.new("Xyz", nil,  "img")
         word2.save()
         Word.clear()
         expect(Word.all).to(eq([]))
@@ -26,9 +29,9 @@ describe '#Word' do
 
   describe('#save') do
     it("saves a word") do
-      word = Word.new("Abc", nil, "blah blah", "img")
+      word = Word.new("Abc", nil, "img")
       word.save()
-      word2 = Word.new("Xyz", nil, "yada yada", "img")
+      word2 = Word.new("Xyz", nil,  "img")
       word2.save()
       expect(Word.all).to(eq([word, word2]))
     end
@@ -36,17 +39,17 @@ describe '#Word' do
 
   describe('#==') do
     it("is the same word if it has the same attributes as another word") do
-      word = Word.new("Abc", nil, "blah blah", "img")
-      word2 = Word.new("Abc", nil, "blah blah", "img")
+      word = Word.new("Abc", nil, "img")
+      word2 = Word.new("Abc", nil, "img")
       expect(word).to(eq(word2))
     end
   end
 
   describe('.find') do
     it("finds a word by id") do
-      word = Word.new("Abc", nil, "blah blah", "img")
+      word = Word.new("Abc", nil, "img")
       word.save()
-      word2 = Word.new("Xyz", nil, "yada yada", "img")
+      word2 = Word.new("Xyz", nil,  "img")
       word2.save()
       expect(Word.find(word.id)).to(eq(word))
     end
@@ -54,9 +57,9 @@ describe '#Word' do
 
   describe('#delete') do
    it("deletes a word by id") do
-     word = Word.new("Abc", nil, "blah blah", "img")
+     word = Word.new("Abc", nil, "img")
      word.save()
-     word2 = Word.new("Xyz", nil, "yada yada", "img")
+     word2 = Word.new("Xyz", nil,  "img")
      word2.save()
      word.delete()
      expect(Word.all).to(eq([word2]))
@@ -65,18 +68,18 @@ describe '#Word' do
 
  describe('#update') do
     it("updates a word by id") do
-      word = Word.new("Abc", nil, "blah blah", "img")
+      word = Word.new("Abc", nil, "img")
       word.save()
-      word.update("Def", "new blah blah", "img")
+      word.update("Def", "img")
       expect(word.word_name).to(eq("Def"))
     end
   end
 
   describe('.search_by_word_name') do
     it("finds a word by word name") do
-      word = Word.new("Abc", nil, "blah blah", "img")
+      word = Word.new("Abc", nil, "img")
       word.save()
-      word2 = Word.new("Xyz", nil, "yada yada", "img")
+      word2 = Word.new("Xyz", nil,  "img")
       word2.save()
       expect(Word.search_by_word_name(word.word_name)).to(eq([word]))
     end
@@ -84,11 +87,21 @@ describe '#Word' do
 
   describe('.alphabetize') do
     it("alphabetizes list of words") do
-      word = Word.new("Xyz", nil, "yada yada", "img")
+      word = Word.new("Xyz", nil,  "img")
       word.save()
-      word2 = Word.new("Abc", nil, "blah blah", "img")
+      word2 = Word.new("Abc", nil, "img")
       word2.save()
       expect(Word.alphabetize).to(eq([word2, word]))
+    end
+  end
+
+  describe('#definitions') do
+    it("returns a word's definition") do
+      word = Word.new("Abc", nil, "img")
+      word.save()
+      definition = Definition.new("yada, yada", word.id, nil)
+      definition.save()
+      expect(word.definitions).to(eq([definition]))
     end
   end
 
