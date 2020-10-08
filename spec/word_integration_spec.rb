f32898131d9da959834e9362a48a5939ec8590e1
Lela Smith <smith.lelaa@gmail.com>
2020-10-08 10:26:52 -0700

    refactor

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -13,13 +13,13 @@ describe('create a word path', {:type => :feature}) do
   end
 end
 
-# describe('create a definition path', {:type => :feature}) do
-#   it('creates a word and then goes to the word page') do
-#     word = Album.new("Abc", nil, "img")
-#     word.save
-#     visit("/words/#{word.id}")
-#     fill_in('definition_text', :with => 'Blah blah')
-#     click_on('Add Definition')
-#     expect(page).to have_content('Blah blah')
-#   end
-# end
+describe('create a definition path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    word = Word.new("Abc", nil, "img")
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('definition_text', :with => 'Blah blah')
+    click_on('Add Definition')
+    expect(page).to have_content('Blah blah')
+  end
+end
